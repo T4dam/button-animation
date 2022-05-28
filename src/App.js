ad6362dd0464b7d764861b44ae9e83e449374d0c
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-28 20:20:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import {motion} from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div style={{width: "100%", height: '100vh', display: 'flex', justifyContent:'center', alignItems: 'center'}}>
+        <motion.button whileHover={{scale:1.1}} whileTap={{scale:0.9}} onClick={()=>null}>Click me</motion.button>
+      </div>
+
   );
 }
 
