35b3d4aebd35364306f3085c403880f4b3c91c88
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-28 21:12:42 +0300

    applied the effects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import {motion} from "framer-motion";
+import Modal from "./components/modal";
+
 
 function App() {
+   
   return (
     
       <div style={{width: "100%", height: '100vh', display: 'flex', justifyContent:'center', alignItems: 'center'}}>
-        <motion.button whileHover={{scale:1.1}} whileTap={{scale:0.9}} onClick={()=>null}>Click me</motion.button>
+        <motion.button
+        whileHover={{scale:1.1}}
+        whileTap={{scale:0.9}}
+        onClick={()=>null}>
+          Click me
+          </motion.button>
       </div>
 
   );
